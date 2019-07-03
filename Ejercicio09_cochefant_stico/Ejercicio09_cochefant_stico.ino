ab4e7fdaa136f02a604319fea9ad64b14b14e071
rodolfoticcosme <rodolfoticcosme@gmail.com>
2019-07-03 15:08:22 +0200

    no message

--- Ejercicio09_cochefant_stico/Ejercicio09_cochefant_stico.ino
+++ Ejercicio09_cochefant_stico/Ejercicio09_cochefant_stico.ino
@@ -10,8 +10,11 @@
 int pinArray[] = {2, 3, 4, 5, 6, 7};
 int count = 0;
 int timer = 100;
+String leido = "";
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Introduce una cadena de texto: ");
   // we make all the declarations at once
   for (count = 0; count < 6; count++) {
     pinMode(pinArray[count], OUTPUT);
@@ -19,7 +22,49 @@ void setup() {
 }
 
 void loop() {
-  timer = analogRead(A0);  //El valor leido por analog read es el temporizador
+  //timer = analogRead(A0);  //El valor leido por analog read es el temporizador
+  
+  int timer = leido.toInt();
+  //coche_fantastico();
+  for (count = 0; count < 6; count++) {
+    //timer = analogRead(A0);
+    digitalWrite(pinArray[count], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count], LOW);
+    delay(timer);
+  }
+  for (count = 5; count >= 0; count--) {
+    timer = analogRead(A0);
+    digitalWrite(pinArray[count], HIGH);
+    delay(timer);
+    digitalWrite(pinArray[count], LOW);
+    delay(timer);
+  }
+  
+  if (Serial.available() > 0) {
+    leido = ""; //vacio lo leido
+    do {
+      char caracter_leido;
+      delay(5);
+      caracter_leido = Serial.read();
+      leido += caracter_leido;
+    }  while (Serial.available() > 0);
+
+    Serial.println("He leido la cadena: \"" + leido + "\"");
+    Serial.println(timer);
+  //cantidad_luz();
+}}
+
+
+
+/*void cantidad_luz()
+{
+  int sensibilidad=0;
+  for (magnitud=0; magnitud
+  
+  }*/
+/*void coche_fantastico()
+{
   for (count = 0; count < 6; count++) {
     timer = analogRead(A0);
     digitalWrite(pinArray[count], HIGH);
@@ -34,4 +79,4 @@ void loop() {
     digitalWrite(pinArray[count], LOW);
     delay(timer);
   }
-}
+}*/
